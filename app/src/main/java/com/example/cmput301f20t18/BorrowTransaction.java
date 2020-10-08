1ae2a543bb29a0514223c90c29617b529d3bc83e
Chase-Warwick <warwick@ualberta.ca>
2020-10-08 16:15:09 -0600

    Added dependancy to google.android.gms.maps.model.LatLng and added BorrowTransaction which uses said dependancy

--- app/src/main/java/com/example/cmput301f20t18/BorrowTransaction.java
+++ app/src/main/java/com/example/cmput301f20t18/BorrowTransaction.java
@@ -1,4 +1,15 @@
 package com.example.cmput301f20t18;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class BorrowTransaction {
+    private LatLng location;
+
+    public LatLng getLocation() {
+        return location;
+    }
+
+    public void setLocation(LatLng location) {
+        this.location = location;
+    }
 }
