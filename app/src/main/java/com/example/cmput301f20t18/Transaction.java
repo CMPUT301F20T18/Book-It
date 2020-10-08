ab6bc400ba46d6e49880ed340c9332c4c7cab5c1
Chase-Warwick <warwick@ualberta.ca>
2020-10-08 16:11:42 -0600

    Added Transaction class variables, getters and setters that were possible without User class implementation

--- app/src/main/java/com/example/cmput301f20t18/Transaction.java
+++ app/src/main/java/com/example/cmput301f20t18/Transaction.java
@@ -1,4 +1,37 @@
 package com.example.cmput301f20t18;
 
-public class Transaction {
+public abstract class Transaction {
+    private String status;
+    private Integer transactionID;
+    private Integer bookID;
+    //Variables bookOwner and bookBorrower will be users.
+    //Currently strings
+    private String bookOwner;
+    private String bookBorrower;
+    //private User bookOwner;
+    //private User bookBorrower;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getTransactionID() {
+        return transactionID;
+    }
+
+    public void setTransactionID(Integer transactionID) {
+        this.transactionID = transactionID;
+    }
+
+    public Integer getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(Integer bookID) {
+        this.bookID = bookID;
+    }
 }
