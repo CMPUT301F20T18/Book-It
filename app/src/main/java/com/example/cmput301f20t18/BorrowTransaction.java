37994074db197ffd9f77ee30f46de3139c05901f
Chase-Warwick <warwick@ualberta.ca>
2020-10-08 16:02:46 -0600

    Added Transaction, RequestTransaction, BorrowTransaction, and ReturnTransaction

--- app/src/main/java/com/example/cmput301f20t18/BorrowTransaction.java
+++ app/src/main/java/com/example/cmput301f20t18/BorrowTransaction.java
@@ -1,5 +1,4 @@
 package com.example.cmput301f20t18;
 
-public class Octagon extends Shape {
-
+public class BorrowTransaction {
 }
--- app/src/main/java/com/example/cmput301f20t18/Transaction.java
+++ app/src/main/java/com/example/cmput301f20t18/Transaction.java
@@ -1,5 +1,4 @@
 package com.example.cmput301f20t18;
 
-public class Triangle extends Shape {
-
+public class Transaction {
 }
