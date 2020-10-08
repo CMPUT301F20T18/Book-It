d2395a9d71da94ad4eca8dabb07eb439e26001a2
Chase-Warwick <warwick@ualberta.ca>
2020-10-08 16:16:37 -0600

    Added variables and getters/setters for them

--- app/src/main/java/com/example/cmput301f20t18/ReturnTransaction.java
+++ app/src/main/java/com/example/cmput301f20t18/ReturnTransaction.java
@@ -1,4 +1,23 @@
 package com.example.cmput301f20t18;
 
-public class ReturnTransaction {
+public class ReturnTransaction extends Transaction {
+    private Boolean BookOwnerScanned;
+    private Boolean BookBorrowerScanned;
+
+
+    public Boolean getBookOwnerScanned() {
+        return BookOwnerScanned;
+    }
+
+    public void setBookOwnerScanned(Boolean bookOwnerScanned) {
+        BookOwnerScanned = bookOwnerScanned;
+    }
+
+    public Boolean getBookBorrowerScanned() {
+        return BookBorrowerScanned;
+    }
+
+    public void setBookBorrowerScanned(Boolean bookBorrowerScanned) {
+        BookBorrowerScanned = bookBorrowerScanned;
+    }
 }
