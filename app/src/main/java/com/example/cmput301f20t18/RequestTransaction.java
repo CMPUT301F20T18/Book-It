a689db04022d65eb45b4ec2cf564281311836bd7
Chase-Warwick <warwick@ualberta.ca>
2020-10-27 14:02:56 -0600

    Tried to make the comments consistent with code base

--- app/src/main/java/com/example/cmput301f20t18/RequestTransaction.java
+++ app/src/main/java/com/example/cmput301f20t18/RequestTransaction.java
@@ -1,23 +1,38 @@
 package com.example.cmput301f20t18;
 
-//All transactions start as RequestTransactions which handle
-//the initial interaction between a book owner and a book borrower
-//Can be accepted or declined
-//If accepted it becomes an ExchangeTransaction and if declined the transaction is deleted
+/**
+ * A RequestTransaction represents a transaction
+ * wherein the borrower has requested to borrow
+ * a book from the book owner
+ * This is where a Transaction starts it's life cycle
+ */
 public class RequestTransaction extends Transaction{
-    //For creating a brand new Transaction
+    /**
+     * This is the constructor for a RequestTransaction
+     * which can be called by developers when a transaction
+     * is created
+     * @param bookOwner The User who owns the book
+     * @param bookBorrower The User who has requested the book
+     * @param bookID The ID of the book being requested
+     */
     public RequestTransaction(User bookOwner, User bookBorrower, Integer bookID) {
         super(bookOwner, bookBorrower, bookID);
     }
-    //For changing status of any transaction back to a RequestTransaction
-    public RequestTransaction(User bookOwner, User bookBorrower, Integer bookId, Integer ID, String status) {
-        super(bookOwner, bookBorrower, bookId, ID, status);
-    }
 
+    /**
+     * Called when the book owner accepts the request
+     * to borrow their book
+     * @return ExchangeTransaction with same ID, bookOwner, bookBorrower and bookID
+     */
     public ExchangeTransaction accept(){
        return (ExchangeTransaction) this.changeStatus("exchange");
     }
 
+    /**
+     * Called when the book owner declines the request
+     * to borrow their book
+     * @return DeclinedTransaction with same ID, bookOwner, bookBorrower and bookID
+     */
     public DeclinedTransaction decline(){
         return (DeclinedTransaction) this.changeStatus("declined");
     }
