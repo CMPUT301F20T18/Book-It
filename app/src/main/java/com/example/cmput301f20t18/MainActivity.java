fb4b09c17275eaba81f4cef83d8804af3b3c17ce
Jacob Deinum <deinum@ualberta.ca>
2020-10-27 15:14:39 -0600

    cleaning master

--- app/src/main/java/com/example/cmput301f20t18/MainActivity.java
+++ app/src/main/java/com/example/cmput301f20t18/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.cmput301f20t18;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
     }
 }
