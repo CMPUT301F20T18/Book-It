9506be71665e86f8831058f7aa1cbb0a40d454a2
Jacob Deinum <deinum@ualberta.ca>
2020-11-04 14:16:15 -0700

    fixing DB

--- app/src/main/java/com/example/cmput301f20t18/HomeScreen.java
+++ app/src/main/java/com/example/cmput301f20t18/HomeScreen.java
@@ -57,6 +57,7 @@ public class HomeScreen extends AppCompatActivity {
             public void onSuccess(List<Task<?>> tasks) {
                 User current = Objects.requireNonNull(retrieve_current_user.getResult()).toObject(User.class);
                 assert(current != null);
+                Log.d(TAG, "onSuccess: " + current.getUsername());
 
                 List<Book> book_results = Objects.requireNonNull(retrieve_books.getResult()).toObjects(Book.class);
                 List<User> user_results = Objects.requireNonNull(retrieve_users.getResult()).toObjects(User.class);
