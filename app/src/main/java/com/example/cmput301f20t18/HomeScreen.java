064296c5a636cb490c19a4af38f5bef406cbb209
Jacob Deinum <deinum@ualberta.ca>
2020-11-04 13:23:49 -0700

    fixing DB issue

--- app/src/main/java/com/example/cmput301f20t18/HomeScreen.java
+++ app/src/main/java/com/example/cmput301f20t18/HomeScreen.java
@@ -58,23 +58,23 @@ public class HomeScreen extends AppCompatActivity {
         Task<QuerySnapshot> retrieve_books = books.get();
         Task<DocumentSnapshot> retrieve_current_user = current_user.get();
 
-        /* phlafoo commented this out because it was causing crash on runtime */
+
         // successfully got all books and users
-       /*Task<List<Task<?>>> combined = Tasks.whenAllComplete(retrieve_books, retrieve_users, retrieve_current_user ).addOnSuccessListener(new OnSuccessListener<List<Task<?>>>() {
+       Task<List<Task<?>>> combined = Tasks.whenAllComplete(retrieve_books, retrieve_users, retrieve_current_user ).addOnSuccessListener(new OnSuccessListener<List<Task<?>>>() {
             @Override
             public void onSuccess(List<Task<?>> tasks) {
                 User current = Objects.requireNonNull(retrieve_current_user.getResult()).toObject(User.class);
                 assert(current != null);
 
+
                 List<Book> book_results = Objects.requireNonNull(retrieve_books.getResult()).toObjects(Book.class);
                 List<User> user_results = Objects.requireNonNull(retrieve_users.getResult()).toObjects(User.class);
-                Log.d(TAG, Integer.toString(book_results.size()));
 
 
             }
 
 
-        });*/
+        });
 
         //* Bottom navigation menu *//*
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
