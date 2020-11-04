1ec9f43bd446b8ef739abe8141d299e9c80da979
Chase-Warwick <warwick@ualberta.ca>
2020-11-03 17:41:20 -0700

    Did some more work on SearchActivity

--- app/src/main/java/com/example/cmput301f20t18/SearchActivity.java
+++ app/src/main/java/com/example/cmput301f20t18/SearchActivity.java
@@ -3,6 +3,7 @@ package com.example.cmput301f20t18;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -17,6 +18,7 @@ public class SearchActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
         EditText searchEditText;
@@ -24,23 +26,33 @@ public class SearchActivity extends AppCompatActivity {
         Button searchButton;
         ListView searchResultList;
 
-        //Temporary Create library to be replaced by user lib access
-        Library lib = new Library();
-
         //Sets up spinner Currently broken. Assigning adapter to spinner causes a crash and debugger
         //complains about source not matching bytecode
         searchSpinner = findViewById(R.id.search_spinner);
         ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(this,
                 R.array.search_spinner_array, android.R.layout.simple_spinner_item);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        //searchSpinner.setAdapter(spinnerAdapter);
+        searchSpinner.setAdapter(spinnerAdapter);
 
         searchEditText = findViewById(R.id.search_edit_text);
 
+        searchButton = findViewById(R.id.search_button);
+        /*
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (searchEditText.getText().toString() != ""){
+                    if (searchSpinner.getPrompt().equals("Books")) {
+                        Log.d("SPINNER", searchSpinner.getPrompt().toString());
+                    }
+                }
+            }
+        });
+         */
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+
+
 
     }
 }
