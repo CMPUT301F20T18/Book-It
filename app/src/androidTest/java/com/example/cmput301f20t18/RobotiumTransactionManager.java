78d966845456a97900385120b0292a7b060aaf92
Johnathon Gil <jggil@ualberta.ca>
2020-11-29 16:47:31 -0700

    Added a function requested by Chase

--- app/src/androidTest/java/com/example/cmput301f20t18/RobotiumTransactionManager.java
+++ app/src/androidTest/java/com/example/cmput301f20t18/RobotiumTransactionManager.java
@@ -1,6 +1,11 @@
 package com.example.cmput301f20t18;
 
+import android.widget.EditText;
+
 import com.robotium.solo.Solo;
+
+import static junit.framework.TestCase.assertTrue;
+
 public class RobotiumTransactionManager {
 
     public static Boolean requestBook(Solo solo, String bookTitle, String searchKey){
@@ -13,4 +18,25 @@ public class RobotiumTransactionManager {
     }
 
 
+    public static void acceptRequestBook (Solo solo, String DEMO_ISBN){
+
+        solo.assertCurrentActivity("Wrong Activity - NOT HOMESCREEN", HomeScreen.class);
+        solo.clickOnView(solo.getView(R.id.tab_scan));
+        solo.assertCurrentActivity("Wrong Activity - NOT SCANNER",
+                Scanner.class);
+        solo.clickOnButton("Manual");
+        solo.enterText((EditText)solo.getView(R.id.manual_isbn_input), DEMO_ISBN);
+        solo.clickOnButton("GO");
+        solo.assertCurrentActivity("Wrong Activity - NOT POST SCAN ACTIVITY",
+                PostScanActivity.class);
+        assertTrue(solo.waitForText(DEMO_ISBN, 1, 2000));
+        solo.clickOnButton("Confirm pick up");
+        solo.assertCurrentActivity("Wrong Activity - NOT SCANNER",
+                Scanner.class);
+        solo.clickOnButton("Manual");
+        solo.enterText((EditText)solo.getView(R.id.manual_isbn_input), DEMO_ISBN);
+        solo.clickOnButton("GO");
+        solo.assertCurrentActivity("Wrong Activity - NOT HOMESCREEN", HomeScreen.class);
+
+    }
 }
