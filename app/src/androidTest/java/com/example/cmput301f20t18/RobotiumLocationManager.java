3e78a930732a215a375505fc841568bd633deb14
Chase-Warwick <warwick@ualberta.ca>
2020-11-29 14:08:52 -0700

    ChooseLocationTest coming along

--- app/src/androidTest/java/com/example/cmput301f20t18/RobotiumLocationManager.java
+++ app/src/androidTest/java/com/example/cmput301f20t18/RobotiumLocationManager.java
@@ -1,4 +1,13 @@
 package com.example.cmput301f20t18;
 
+import com.robotium.solo.Solo;
+
 public class RobotiumLocationManager {
+    public static void deleteAll(Solo solo){
+        solo.assertCurrentActivity("Wrong Activity - NOT CHOOSELOCATIONACTIVITY",
+                ChooseLocationActivity.class);
+        while(solo.waitForText("Select", 1, 2000)){
+            solo.clickOnView(solo.getView(R.id.button_delete_location));
+        }
+    }
 }
--- app/src/androidTest/java/com/example/cmput301f20t18/RobotiumUserBookManager.java
+++ app/src/androidTest/java/com/example/cmput301f20t18/RobotiumUserBookManager.java
@@ -1,5 +1,8 @@
 package com.example.cmput301f20t18;
 
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 
 import com.robotium.solo.Solo;
@@ -26,10 +29,36 @@ public class RobotiumUserBookManager {
     public static void deleteAll(Solo solo){
         solo.assertCurrentActivity("Wrong Activity - NOT HOMESCREEN", HomeScreen.class);
         solo.clickOnView(solo.getView(R.id.tab_mybooks));
-        while (solo.searchText("ISBN", 1)) {
-            solo.clickOnView(solo.getView(R.id.button_book_more));
-            solo.clickOnButton("Delete");
-            solo.clickOnButton("Delete");
+
+        ViewGroup tabs = (ViewGroup) solo.getView(R.id.mybooks_tab_layout);
+        Log.d("ROBO", tabs.toString());
+        for (int i=0; i < tabs.getChildCount(); i++) {
+            //deleteAllInTab(solo, (ViewGroup) tabs.getChildAt(i));
+        }
+            //deleteAllInTab(solo, (ViewGroup) tabs.getChildAt(i));
+
+//            solo.sleep(2000);
+            while (solo.searchText("ISBN", 1)) {
+                View view = solo.getView(R.id.button_book_more);
+                Log.d("ROBO", view.getParent().getParent().getParent().getParent().getParent().getParent().getParent().toString());
+                solo.clickOnView(solo.getView(R.id.button_book_more));
+                solo.clickOnButton("Delete");
+                solo.clickOnButton("Delete");
+            }
+        //}
+    }
+
+    private static void deleteAllInTab(Solo solo, ViewGroup tab) {
+        Log.d("ROBO", tab.toString());
+        for (int i=0; i < tab.getChildCount(); i++){
+            deleteBook(solo, (ViewGroup) tab.getChildAt(i));
+        }
+    }
+
+    private static void deleteBook(Solo solo, ViewGroup book) {
+        Log.d("ROBO", book.toString());
+        for (int i=0; i < book.getChildCount(); i++){
+            Log.d("ROBO", book.getChildAt(i).toString());
         }
     }
 }
